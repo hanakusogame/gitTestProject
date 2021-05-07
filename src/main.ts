6d9459fe7a774851b94e8dfbf5d1f398bf28647c
hanakusogame <jjipqw@gmail.com>
2021-05-08 07:27:58 +0900

    プレイヤーを右側に移動

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ function main(param: g.GameMainParameterObject): void {
 		});
 
 		// プレイヤーの初期座標を、画面の中心に設定します
-		player.x = 100;
+		player.x = 1000;
 		player.y = (g.game.height - player.height) / 2;
 		player.onUpdate.add(() => {
 			// 毎フレームでY座標を再計算し、プレイヤーの飛んでいる動きを表現します
